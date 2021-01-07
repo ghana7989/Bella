5719ea40ac0932a4a6397afe2ebbe25075483589
ghana <ghana.chindukuri@gmail.com>
2021-01-07 21:16:02 +0530

    adding movement to header Images

--- js/main.js
+++ js/main.js
@@ -40,11 +40,36 @@ function initNavigation() {
 function initHeaderTilt() {
   const Header = document.querySelector("header")
   Header.addEventListener('mousemove', moveImages)
+
 }
 
 function moveImages(e) {
   const { offsetX, offsetY, target } = e;
   const { clientWidth, clientHeight } = target
+
+  // we will get 0,0 when we are in center
+  // extremities are [-.5,.5]
+  const xPos = offsetX / clientWidth - 0.5
+  const yPos = offsetY / clientHeight - 0.5
+  const leftImages = gsap.utils.toArray(".hg__image")
+  const modifier = index => index * 2 + .5
+  leftImages.forEach((image, index) => {
+    gsap.to(image,
+      {
+        duration: 1.2,
+        x: xPos * 20 * modifier(index * 0.5),
+        y: yPos * 30 * modifier(index * 0.5),
+        rotationX: yPos * 10,
+        rotationY: xPos * 40,
+        ease: "Power3.out"
+      })
+  })
+  gsap.to('.decor__circle', {
+    duration: 1.7,
+    x: 100 * xPos,
+    y: 120 * yPos,
+    ease: "Power4.out"
+  })
 }
 
 function init() {
